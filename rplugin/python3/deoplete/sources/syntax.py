4554232170f95a255253903372605d103e881520
LittleBird <ilms49898723@gmail.com>
2019-08-06 02:25:48 +0900

    Fix typing

--- rplugin/python3/deoplete/sources/syntax.py
+++ rplugin/python3/deoplete/sources/syntax.py
@@ -4,23 +4,26 @@
 # License: MIT license
 #=============================================================================
 
-from .base import Base
+import typing
 
-import deoplete.util
+from deoplete.base.source import Base
+from deoplete.util import parse_buffer_pattern, getlines
+from deoplete.util import Nvim, UserContext, Candidates
+
 
 class Source(Base):
-    def __init__(self, vim):
-        Base.__init__(self, vim)
+    def __init__(self, vim: Nvim) -> None:
+        super().__init__(vim)
 
         self.name = 'syntax'
         self.mark = '[S]'
-        self.__include_files = {}
+        self.included_syntax = {}
         self.vim.call('necosyntax#initialize')
 
-    def on_event(self, context):
-        self.__include_files[context['filetype']] = [
+    def on_event(self, context: UserContext) -> None:
+        self.included_syntax[context['filetype']] = [
             { 'word': x } for x in
             self.vim.call('necosyntax#gather_candidates')]
 
-    def gather_candidates(self, context):
-        return self.__include_files.get(context['filetype'], [])
+    def gather_candidates(self, context: UserContext) -> Candidates:
+        return self.included_syntax.get(context['filetype'], [])
